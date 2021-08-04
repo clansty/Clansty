19b35a980829e4362de98447ff1832c2a75934f1
Clansty <i@gao4.pw>
2021-08-04 14:25:17 +0800

    差不多有个样子

---
+++ components/LinkEntries.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/LinkEntries.module.scss'
+
+export default ()=>(
+    <div className={styles.container}>
+        <div className={styles.title}>
+            你好，这里是凌莞
+        </div>
+        <a href="">Blog</a>
+    </div>
+)
