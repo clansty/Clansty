a7124fc4db01a89e2911c5bc2c08653f9556d3ca
Clansty <i@gao4.pw>
2021-08-04 17:21:09 +0800

    移动端适配

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.scss'
 import classNames from 'classnames'
 import LinkEntries from '../components/LinkEntries'
 import randomChoose from '../utils/randomChoose'
+import Image from 'next/image'
 
 export default function Home() {
     const emojiList = ['(≧▽≦)', '( ╹▽╹ )', '(・∀・)']
@@ -10,18 +11,28 @@ export default function Home() {
         <div className={classNames(styles.container, styles.background1)}>
             <Head>
                 <title>凌莞{randomChoose(emojiList)}喵～</title>
-                <link rel="icon" href="/favicon.ico" />
             </Head>
 
             {LinkEntries()}
 
-            {process.env.BUILD_FOR_DOMESTIC && <footer className={styles.footer}>
+            <footer className={styles.footer}>
+                {process.env.BUILD_FOR_DOMESTIC &&
                 <a href="https://beian.miit.gov.cn/" target="_blank" className={styles.beian}>
                     <small>
                         苏ICP备2020048816号-1
                     </small>
+                </a>}
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Hosted on {' '}
+                    <span className={styles.logo}>
+                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16}/>
+                    </span>
                 </a>
-            </footer>}
+            </footer>
         </div>
     )
 }
