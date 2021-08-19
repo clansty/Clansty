599f82c50cd4786ab70d8633dd4eb379cc661e7f
凌莞 <i@gao4.pw>
2021-08-19 11:43:37 +0800

    进行了一个邮箱地址的混淆

--- components/SocialNetworks.tsx
+++ components/SocialNetworks.tsx
@@ -1,10 +1,13 @@
-import {GithubOutlined, TwitterOutlined} from '@ant-design/icons'
+import {GithubOutlined, MailOutlined, TwitterOutlined} from '@ant-design/icons'
 import {GnuPGIcon, TelegramIcon} from './IconSvgs'
 import Link from 'next/link'
 
 const SocialNetworks = () => {
     return (
         <>
+            <a onClick={sendMail}>
+                <MailOutlined/>
+            </a>
             <a href="https://github.com/Clansty">
                 <GithubOutlined/>
             </a>
@@ -24,3 +27,16 @@ const SocialNetworks = () => {
 }
 
 export default SocialNetworks
+
+function sendMail() {
+    const emptyObj = {}
+    // @ts-ignore
+    const part1 = `${emptyObj.nonExist}`[5]
+    const part2 = String.fromCharCode(Math.pow(2, 6))
+    const part3 = atob('Z2FvNC5wdw==')
+    const proto = atob('bWFpbHRvOg==')
+    const adr = `${proto}${part1}${part2}${part3}`
+    const link = document.createElement('a')
+    link.href = adr
+    link.click()
+}
